d68f4bcb2324af8e108686ba91d005248bdee24f
totuslink <summerbud@totuslink.com>
2021-07-16 14:21:08 +0800

    Config index

---
+++ pages/index.jsx
@@ -0,0 +1,16 @@
+import Header from '../components/Header'
+import SectionContainer from '../components/SectionContainer'
+
+export default function Home() {
+  return (
+    <div
+      className="bg-sd-brwhite w-screen h-screen"
+    >
+      <>
+        <SectionContainer>
+          <Header />
+        </SectionContainer>
+      </>
+    </div>
+  )
+}
