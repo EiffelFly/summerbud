61f93548c62b2eb85021b0780668f55672984fe5
totuslink <summerbud@totuslink.com>
2021-07-16 14:20:53 +0800

    Config next project

---
+++ next.config.js
@@ -0,0 +1,20 @@
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
+module.exports = withBundleAnalyzer({
+  pageExtensions: ["js", "jsx", "mdx"],
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.mdx/,
+      use: [
+        options.defaultLoaders.babel,
+        {
+          loader: "@mdx-js/loader",
+        },
+      ],
+    });
+
+    return config;
+  },
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,26 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'sd-brblack': '#002B36',
+        'sd-black': '#073642',
+        'sd-brgreen': '#586e75',
+        'sd-bryellow': '#657b83',
+        'sd-brblue': '#839496',
+        'sd-brcyan': '#93a1a1',
+        'sd-white': '#eee8d5',
+        'sd-brwhite': '#fdf6e3',
+        'sd-yellow': '#b58900',
+        'sd-orange': '#cb4b16',
+        'sd-red': '#d30102',
+        'sd-magenta': '#d33682',
+        'sd-violet': '#6c71c4',
+        'sd-blue': '#268bd2',
+        'sd-cyan': '#2aa198',
+        'sd-green': '#859900',
+      },
+    },
   },
   variants: {
     extend: {},
