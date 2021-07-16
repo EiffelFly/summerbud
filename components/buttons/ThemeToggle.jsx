79fa5582b3c40a6111f44464e6a8d726a8b169e6
totuslink <summerbud@totuslink.com>
2021-07-16 16:11:59 +0800

    Succefully toggle dark mode

---
+++ components/buttons/ThemeToggle.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { ThemeContext } from "../../theme/ThemeContext";
+import MoonStarsFillIcon from "../icons/MoonStarsFillIcon";
+import SunFillIcon from "../icons/SunFillIcon";
+
+const ThemeToggle = () => {
+  const { theme, setTheme } = React.useContext(ThemeContext);
+
+  return (
+    <div
+      className="rounded-full p-2 flex "
+      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+    >
+      {theme === "dark"
+        ? <SunFillIcon size={5} />
+        : <MoonStarsFillIcon size={5} />
+      }
+    </div>
+  );
+};
+
+export default ThemeToggle;
