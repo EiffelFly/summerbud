9a6be383238bab3e7a3a0d5390065f03090885bf
totuslink <summerbud@totuslink.com>
2021-07-18 10:28:49 +0800

    Add translation file

---
+++ locales/translation.js
@@ -0,0 +1,16 @@
+const translation = {
+  "en": {
+    components: {
+      AboutButton: "About",
+      SeriesButton: "Series"
+    }
+  },
+  "zh-tw": {
+    components: {
+      AboutButton: "關於我",
+      SeriesButton: "系列文章"
+    }
+  }
+};
+
+export default translation;
