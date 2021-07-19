27fb94f477598993c09cce1fae0af70a5720e7c8
totuslink <summerbud@totuslink.com>
2021-07-19 17:11:22 +0800

    Not get component from mdx directly by substitute it from getMDXComponent

--- pages/[lang]/[slug].jsx
+++ pages/[lang]/[slug].jsx
@@ -1,29 +1,41 @@
-import { getAllSlugs } from "../../lib/files";
+import { getAllSlugs, getContent } from "../../lib/files";
+import { useMemo } from "react";
+import { getMDXComponent } from "mdx-bundler/client";
+import SectionContainer from "../../components/SectionContainer";
+import Header from "../../components/Header";
+import CustomLink from "../../components/CustomLink";
+import Footer from "../../components/Footer";
 
-
-const Pages = () => {
+const MDXPages = ({ code }) => {
+  const Component = useMemo(() => getMDXComponent(code), [code]);
   return (
-    <div>
-      hi
+    <div className="bg-sd-brwhite dark:bg-sd-brblack w-screen min-h-screen">
+      <SectionContainer>
+        <Header />
+        <article className="prose prose-lg py-12 dark:prose-dark">
+          <Component components={{ CustomLink: CustomLink }} />
+        </article>
+        <Footer />
+      </SectionContainer>
     </div>
-  )
-}
+  );
+};
 
-export const getStaticProps = () => {
+export const getStaticProps = async ({ params }) => {
+  const { slug, code } = await getContent(params.slug, params.lang, "page");
   return {
-    props: {}
-  }
-}
+    props: {
+      code,
+    },
+  };
+};
 
 export const getStaticPaths = () => {
   const paths = getAllSlugs("page");
-  console.log(paths)
   return {
     paths,
     fallback: false,
   };
-}
+};
 
-
-
-export default Pages;
+export default MDXPages;
