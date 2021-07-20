23a8a397d73b39880255995f57a8e5fe559f24d5
totuslink <summerbud@totuslink.com>
2021-07-20 14:12:00 +0800

    Add summerbud title at header

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,10 +4,18 @@ import SeriesButton from "./buttons/SeriesButton"
 import ThemeToggle from "./buttons/ThemeToggle";
 
 const Header = () => {
+
+  const title = "{ summerbud }"
+
   return (
     <header className="flex flex-row py-8">
-      <div className="flex flex-row items-center mr-auto">
+      <div className="flex flex-row items-center mr-auto gap-x-6">
         <SummerbudAvatar size="45" />
+        <div
+          className="font-sans font-semibold text-sd-brgreen dark:text-sd-brcyan text-xl"
+        >
+          {title}
+        </div>
       </div>
       <div className="flex flex-row items-center ml-auto gap-x-8">
         <AboutButton />
