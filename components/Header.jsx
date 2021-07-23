5ee82c469b44f16a4aa1d06f08ea803a187f63f6
totuslink <summerbud@totuslink.com>
2021-07-23 10:47:49 +0800

    Add responsive navbar design

--- components/Header.jsx
+++ components/Header.jsx
@@ -9,35 +9,59 @@ import TranslationDropdown from "./TranslationDropdown";
 import { useRouter } from "next/dist/client/router";
 import LessenLearnedButton from "./buttons/LessenLearnedButton";
 import SnippetButton from "./buttons/SnippetButton";
+import MapIcon from "./icons/MapIcon";
+import { useState } from "react";
+import XIcon from "./icons/XIcon";
 const Header = () => {
   const title = "{ summerbud }";
   const { locale } = useTranslation();
   const { route } = useRouter();
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <header className="flex flex-row py-8">
-      <CustomLink href={"/"}>
-        <a className="flex flex-row items-center mr-auto gap-x-6">
-          <SummerbudAvatar size="45" />
-          <div className="font-sans font-semibold text-sd-brgreen dark:text-sd-brcyan text-xl">
-            {title}
-          </div>
-        </a>
-      </CustomLink>
+    <header className="flex flex-col xl:flex-row py-8">
+      <div className="flex flex-row mb-8">
+        <CustomLink href={"/"}>
+          <a className="flex flex-row items-center mr-auto gap-x-6">
+            <SummerbudAvatar size="45" />
+            <div className="font-sans font-semibold text-sd-brgreen dark:text-sd-brcyan text-lg sm:text-xl">
+              {title}
+            </div>
+          </a>
+        </CustomLink>
+        <button
+          className="xl:hidden my-auto ml-auto w-8 h-8 border border-sd-brgreen dark:border-sd-brcyan rounded-md"
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          {isOpen ? <XIcon size={6} /> : <MapIcon size={4} />}
+        </button>
+      </div>
 
-      <div className="flex flex-row items-center ml-auto gap-x-8">
+      <div className="hidden xl:flex xl:flex-row items-center ml-auto gap-x-8">
         <AboutButton />
         <SeriesButton />
         <PostsButton />
         <LessenLearnedButton />
         <SnippetButton />
-        <div
-          className="flex flex-row gap-x-4 ml-8"
-        >
-          { !route.includes("posts") && <TranslationDropdown />}
+        <div className="flex flex-row gap-x-4 ml-8">
+          {!route.includes("posts") && <TranslationDropdown />}
           <ThemeToggle />
         </div>
       </div>
+
+      {isOpen && (
+        <div className="flex flex-col xl:hidden gap-y-4">
+          <AboutButton />
+          <SeriesButton />
+          <PostsButton />
+          <LessenLearnedButton />
+          <SnippetButton />
+          <div className="flex flex-col gap-y-4 xl:gap-x-4 justify-items-start items-start mt-8">
+            {!route.includes("posts") && <TranslationDropdown />}
+            <ThemeToggle />
+          </div>
+        </div>
+      )}
     </header>
   );
 };
