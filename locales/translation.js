c8032f67e998b23a1bc8696ee5b271a2ad7bb006
totuslink <summerbud@totuslink.com>
2021-08-01 11:50:34 +0800

    Add footer links translation

--- locales/translation.js
+++ locales/translation.js
@@ -15,12 +15,50 @@ const translation = {
       subscriptionButton: "Subscribe",
       subscriptionClarification: "I'll only send emails when new content is posted. No spam.",
       subscriptionDuplicationWarning: "You already subscribed to my mailing list.",
+      
     },
     pages: {
       main: {
         headline: "Summerbud's writing",
         subHeadline: "This blog is mostly about literature, programming and entrepreneurship. Hopefully, every reader will leave with something worth noting. In that spirit, each article here is as concise as possible and contains actionable content."
       }
+    },
+    footer: {
+      pages: {
+        _title: "Pages",
+        about: "About",
+        posts: "Posts",
+        series: "Series",
+        snippets: "Snippets",
+        lessenLearned: "Lessen Learned"
+      },
+      series: {
+        _title: "Series",
+        nextjs: "Blogging with Next.js",
+        builder: "The builder's life",
+        endOfLine: "The other end of line",
+        literature: "The master degree of Taiwanese literature"
+      },
+      novels:{
+        _title: "Novels",
+        rat: "The rat",
+        beast: "The beast",
+        breakfast: "A boiled egg for breakfast"
+      },
+      projects: {
+        _title: "Projects",
+        working: "What I am currently working on",
+        totuslink: "Totuslink",
+        hologram: "Hologram",
+        hololink: "Hololink",
+        pnyx: "Pnyx",
+      },
+      contact: {
+        _title: "Contact",
+        email: "Email",
+        twitter: "Twitter",
+        github: "Github",
+      },
     }
   },
   "zh-tw": {
@@ -39,12 +77,50 @@ const translation = {
       subscriptionButton: "Subscribe",
       subscriptionClarification: "我只會在新內容刊登時寄送信件給您，拒絕垃圾信件。",
       subscriptionDuplicationWarning: "這個電子信箱已經訂閱。",
+      
     },
     pages: {
       main: {
         headline: "Summerbud 的部落格",
         subHeadline: "這個部落格的內容大多關於文學、程式設計及創業。希望每個讀者離開時都能帶走一些值得收藏的事物，秉持這種精神，這裡的每篇文章都盡可能簡潔，且包含了可以實作的內容。"
       }
+    },
+    footer: {
+      pages: {
+        _title: "頁面",
+        about: "關於我",
+        posts: "文章",
+        series: "系列",
+        snippets: "Snippets",
+        lessenLearned: "Lessen Learned"
+      },
+      series: {
+        _title: "系列",
+        nextjs: "以 Nextjs 開啟的部落格之旅",
+        builder: "The builder's life",
+        endOfLine: "電話的另一端",
+        literature: "台文所與顛倒世界"
+      },
+      novels:{
+        _title: "小說",
+        rat: "耗子",
+        beast: "獸",
+        breakfast: "早餐一顆水煮蛋"
+      },
+      projects: {
+        _title: "專案",
+        working: "我在做什麼",
+        totuslink: "Totuslink",
+        hologram: "Hologram",
+        hololink: "Hololink",
+        pnyx: "Pnyx",
+      },
+      contact: {
+        _title: "聯絡",
+        email: "Email",
+        twitter: "Twitter",
+        github: "Github",
+      },
     }
   }
 };
