2fcbffcf59942970111bb6269146cf063ea279ce
totuslink <summerbud@totuslink.com>
2021-08-03 15:10:58 +0800

    Change series articles list component border color

--- components/SeriesArticlesList.jsx
+++ components/SeriesArticlesList.jsx
@@ -7,7 +7,7 @@ const SeriesArticlesList = ({ articles, className }) => {
         <CustomLink href={article.path} key={article.path}>
           <div
             className={
-              "flex flex-col gap-y-4 font-sans rounded-lg bg-sd-white dark:bg-sd-black p-6 border border-sd-black dark:border-sd-white border-opacity-0 dark:border-opacity-0 hover:border-opacity-100 dark:hover:border-opacity-100 cursor-pointer"
+              "flex flex-col gap-y-4 font-sans rounded-lg bg-sd-white dark:bg-sd-black p-6 border border-sd-brgreen dark:border-sd-brcyan border-opacity-0 dark:border-opacity-0 hover:border-opacity-100 dark:hover:border-opacity-100 cursor-pointer"
             }
           >
             <div className="text-lg text-sd-black dark:text-sd-white font-semibold">
