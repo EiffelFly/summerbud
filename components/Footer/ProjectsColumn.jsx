599c9f13a86529ce974457c4a46bc765a5ce2dce
totuslink <summerbud@totuslink.com>
2021-08-09 16:39:31 +0800

    Add township and delete old project from footer project list

--- components/Footer/ProjectsColumn.jsx
+++ components/Footer/ProjectsColumn.jsx
@@ -11,21 +11,25 @@ const ProjectsColumn = () => {
         url: "/projects",
       },
       {
+        title: t("footer.projects.township"),
+        url: "/projects/township"
+      },
+      {
         title: t("footer.projects.totuslink"),
         url: "/projects/totuslink",
       },
-      {
-        title: t("footer.projects.hologram"),
-        url: "/projects/hologram",
-      },
-      {
-        title: t("footer.projects.hololink"),
-        url: "/projects/hololink",
-      },
-      {
-        title: t("footer.projects.pnyx"),
-        url: "/projects/pnyx",
-      },
+      // {
+      //   title: t("footer.projects.hologram"),
+      //   url: "/projects/hologram",
+      // },
+      // {
+      //   title: t("footer.projects.hololink"),
+      //   url: "/projects/hololink",
+      // },
+      // {
+      //   title: t("footer.projects.pnyx"),
+      //   url: "/projects/pnyx",
+      // },
     ],
   };
   return (
