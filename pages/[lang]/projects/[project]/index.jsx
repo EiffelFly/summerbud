ce8c4f4cdc936f05950607779584b46af6f22cbb
totuslink <summerbud@totuslink.com>
2021-08-10 15:05:49 +0800

    Finish projects/[project]/index

--- pages/[lang]/projects/[project]/index.jsx
+++ pages/[lang]/projects/[project]/index.jsx
@@ -5,13 +5,16 @@ import PageSeo from "../../../../components/PageSeo";
 import Header from "../../../../components/Header";
 import Footer from "../../../../components/Footer";
 import {
-  getProjectIndexContent,
-  getProjectIndexPath,
+  getAllProjectPath,
+  getProjectContent,
+  getProjectDevlogMeta,
 } from "../../../../lib/files";
 import CustomLink from "../../../../components/CustomLink";
 import { useMemo } from "react";
+import DevlogsList from "../../../../components/DevlogsList";
+import DevLogsIntro from "../../../../components/DevlogsIntro";
 
-const MDXPage = ({ code, metadata }) => {
+const MDXPage = ({ code, metadata, devlogs }) => {
   const Component = useMemo(() => getMDXComponent(code), [code]);
   return (
     <div className="bg-sd-brwhite dark:bg-sd-brblack w-screen min-h-screen">
@@ -20,6 +23,8 @@ const MDXPage = ({ code, metadata }) => {
         <Header hasTranslation={true} />
         <article className="md:mx-auto prose prose-lg py-12 dark:prose-dark">
           <Component components={{ CustomLink: CustomLink }} />
+          <DevLogsIntro />
+          <DevlogsList devlogs={devlogs} />
         </article>
         <Footer />
       </SectionContainer>
@@ -28,20 +33,24 @@ const MDXPage = ({ code, metadata }) => {
 };
 
 export const getStaticProps = async ({ params }) => {
-  const { code, metadata } = await getProjectIndexContent({
+  const { code, metadata } = await getProjectContent({
+    isIndex: true,
     project: params.project,
     lang: params.lang,
   });
+  const devlogs = await getProjectDevlogMeta({ projectName: params.project })
+  console.log(devlogs)
   return {
     props: {
       code,
       metadata,
+      devlogs
     },
   };
 };
 
 export const getStaticPaths = async () => {
-  const paths = getProjectIndexPath();
+  const paths = getAllProjectPath({isIndex: true});
   return {
     paths: paths,
     fallback: false,
