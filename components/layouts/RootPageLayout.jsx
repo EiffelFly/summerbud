cc364e7963893c5c2c75e5c8567e0a3d8473afe4
totuslink <summerbud@totuslink.com>
2021-08-29 22:28:05 +0800

    Fix page og:url issue

--- components/layouts/RootPageLayout.jsx
+++ components/layouts/RootPageLayout.jsx
@@ -2,12 +2,14 @@ import SectionContainer from "../SectionContainer";
 import PageSeo from "../PageSeo";
 import Header from "../Header";
 import Footer from "../Footer";
+import useTranslation from "../../hooks/useTranslation";
 
 const RootPageLayout = ({ metadata, children }) => {
+  const { locale } = useTranslation();
   return (
     <div className="bg-sd-brwhite dark:bg-sd-brblack w-screen min-h-screen">
       <SectionContainer gap="gap-y-16">
-        <PageSeo metadata={metadata} />
+        <PageSeo metadata={metadata} locale={locale} />
         <Header hasTranslation={metadata.hasTranslation} />
         <article className="md:mx-auto prose prose-lg py-12 dark:prose-dark">
           {children}
