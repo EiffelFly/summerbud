4ace0d552766b10f7182af9030588af76c94204e
totuslink <summerbud@totuslink.com>
2021-08-31 16:17:33 +0800

    🎉 feat(optimization): merge index with[slug]

--- pages/[lang]/projects/[project]/[slug].jsx
+++ pages/[lang]/projects/[project]/[slug].jsx
@@ -1,8 +1,12 @@
-import { useMemo } from "react";
+import { useMemo, Fragment } from "react";
 import { getMDXComponent } from "mdx-bundler/client";
 import Header from "../../../../components/Header";
 import SectionContainer from "../../../../components/SectionContainer";
-import { constructPathParams, getAllArticleContent } from "../../../../lib/files";
+import {
+  constructPathParams,
+  getAllArticleContent,
+  getProjectDevlogMeta,
+} from "../../../../lib/files";
 import Footer from "../../../../components/Footer";
 import CustomLink from "../../../../components/CustomLink";
 import PostTitle from "../../../../components/PostTitle";
@@ -10,10 +14,12 @@ import SubscriptionForm from "../../../../components/SubscriptionForm";
 import CustomImage from "../../../../components/CustomImage";
 import PostSeo from "../../../../components/PostSeo";
 import useTranslation from "../../../../hooks/useTranslation";
+import GeneralList from "../../../../components/GeneralList";
+import GeneralListIntro from "../../../../components/GeneralListIntro";
 
-const MDXPages = ({ code, metadata }) => {
+const MDXPages = ({ code, metadata, devlogs }) => {
   const Component = useMemo(() => getMDXComponent(code), [code]);
-  const { locale } = useTranslation();
+  const { t, locale } = useTranslation();
   return (
     <div className="bg-sd-brwhite dark:bg-sd-brblack w-screen min-h-screen">
       <SectionContainer gap={"gap-y-16"}>
@@ -28,6 +34,12 @@ const MDXPages = ({ code, metadata }) => {
           <Component
             components={{ CustomLink: CustomLink, Image: CustomImage }}
           />
+          {devlogs.length > 0 && (
+            <Fragment>
+              <GeneralListIntro>{t("components.devlogs")}</GeneralListIntro>
+              <GeneralList elements={devlogs} />
+            </Fragment>
+          )}
           <SubscriptionForm className={"mt-80"} />
         </article>
         <Footer />
@@ -37,22 +49,27 @@ const MDXPages = ({ code, metadata }) => {
 };
 
 export const getStaticProps = async ({ params }) => {
-
   const { code, metadata } = await getAllArticleContent({
     type: "projects",
-    pathTail: `${params.project}/${params.slug}/index.${params.lang}.mdx`
-  })
+    pathTail: `${params.project}/${params.slug}/index.${params.lang}.mdx`,
+  });
+
+  let devlogs = [];
+  if (params.slug === "outline") {
+    devlogs = await getProjectDevlogMeta({ projectName: params.project });
+  }
 
   return {
     props: {
       code,
       metadata,
+      devlogs,
     },
   };
 };
 
 export const getStaticPaths = () => {
-  const paths = constructPathParams("projects")
+  const paths = constructPathParams("projects");
   return {
     paths,
     fallback: false,
