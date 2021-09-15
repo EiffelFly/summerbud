c9c581ddee56008ec31755402575a772dca424ff
totuslink <summerbud@totuslink.com>
2021-09-15 16:50:52 +0800

    🔧 fix(seo): fix seo url

--- pages/[lang]/how-to/[slug].jsx
+++ pages/[lang]/how-to/[slug].jsx
@@ -20,7 +20,7 @@ const MDXPages = ({ code, metadata }) => {
   return (
     <div className="bg-sd-brwhite dark:bg-sd-brblack w-screen min-h-screen">
       <SectionContainer gap={"gap-y-16"}>
-        <PostSeo metadata={metadata} locale={locale} type={"projects"} />
+        <PostSeo metadata={metadata} locale={locale} type={"how-to"} />
         <Header hasTranslation={metadata.hasTranslation} />
         <article className="md:mx-auto prose prose-lg py-12 dark:prose-dark">
           <PostTitle
