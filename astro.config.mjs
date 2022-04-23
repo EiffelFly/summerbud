60fcedffb9fd7bb443cf8c55b9eb40a6fa9a0de9
pochun-chiu <pochun.chiu@instill.tech>
2022-04-23 15:26:05 +0800

    feat: setup tailwindCSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
-import { defineConfig } from 'astro/config';
-  import solid from '@astrojs/solid-js';
+import { defineConfig } from "astro/config";
+import solid from "@astrojs/solid-js";
+
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     solid(),
-  ]
+    tailwind({
+      config: { path: "./custom-config.js" },
+    }),
+  ],
 });
