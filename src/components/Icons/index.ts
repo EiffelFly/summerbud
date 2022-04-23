b91a5b7cf491c34d32ba230a103154bab4b751b6
pochun-chiu <pochun.chiu@instill.tech>
2022-04-23 17:06:47 +0800

    fix: fix type import typo

--- src/components/Icons/index.ts
+++ src/components/Icons/index.ts
@@ -3,7 +3,7 @@ import type { GithubIconProps } from "./GithubIcon";
 import MoonStarsIcon from "./MoonStarsIcon";
 import type { MoonStarsIconProps } from "./MoonStarsIcon";
 import SunIcon from "./SunIcon";
-import { SunIconProps } from "./SunIcon";
+import type { SunIconProps } from "./SunIcon";
 import TwitterIcon from "./TwitterIcon";
 import type { TwitterIconProps } from "./TwitterIcon";
 
