15ebf0905ced73f71adaec65c7526696742f45e0
pochun-chiu <pochun.chiu@instill.tech>
2022-04-27 11:28:03 +0800

    feat: toggle giscus theme

--- src/components/Buttons/ThemeToggle/ThemeToggle.tsx
+++ src/components/Buttons/ThemeToggle/ThemeToggle.tsx
@@ -9,14 +9,34 @@ export interface ThemeToggleProps {
 const ThemeToggle: Component<ThemeToggleProps> = ({ styleName }) => {
   const [isDark, setIsDark] = createSignal(true);
 
+  const sendMessageToGiscus = (message) => {
+    const iframe = document.querySelector(
+      "iframe.giscus-frame"
+    ) as HTMLIFrameElement;
+    if (!iframe) return;
+    iframe.contentWindow.postMessage({ giscus: message }, "https://giscus.app");
+  };
+
   const toggleTheme = () => {
-    console.log("hi");
+    // Discus solution from:
+    // https://github.com/giscus/giscus/issues/336#issuecomment-1007922777
+
     const html = document.querySelector("html");
     if (isDark()) {
       html.classList.remove("dark");
+      sendMessageToGiscus({
+        setConfig: {
+          theme: "light",
+        },
+      });
       setIsDark(false);
     } else {
       html.classList.add("dark");
+      sendMessageToGiscus({
+        setConfig: {
+          theme: "dark",
+        },
+      });
       setIsDark(true);
     }
   };
