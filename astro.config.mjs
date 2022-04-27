c79defae5de13140a52cc0f0fd353c028c6b1d4f
pochun-chiu <pochun.chiu@instill.tech>
2022-04-27 22:42:33 +0800

    feat: add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,20 @@
 import { defineConfig } from "astro/config";
 import solid from "@astrojs/solid-js";
-
 import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://www.summerbud.org",
   integrations: [
     solid(),
     tailwind({
-      config: { path: "./tailwind.config.cjs" },
+      config: {
+        path: "./tailwind.config.cjs",
+      },
     }),
+    sitemap(),
   ],
   markdown: {
     // Can be 'shiki' (default), 'prism' or false to disable highlighting
