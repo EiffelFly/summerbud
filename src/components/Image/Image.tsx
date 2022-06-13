c1bd03b6276fc61ddb0e25aa44ed24ac446ae265
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-06-13 12:23:04 +0800

    feat: update Img component

--- src/components/Image/Image.tsx
+++ src/components/Image/Image.tsx
@@ -1,15 +1,39 @@
 import { Component } from "solid-js";
 import cn from "clsx";
+import { Nullable } from "../../types/general";
 
 export type ImageProps = {
-  src: string;
+  src: Nullable<string>;
   width: string;
   height: string;
-  alt: string;
+  alt: Nullable<string>;
+  marginBottom: Nullable<string>;
+  source: Nullable<string>;
 };
 
-const Image: Component<ImageProps> = ({ src, width, height, alt }) => {
-  return <img src={src} class={cn(width, height)} alt={alt} />;
+const Image: Component<ImageProps> = ({
+  src,
+  width,
+  height,
+  alt,
+  source,
+  marginBottom,
+}) => {
+  if (!src) return null;
+  return (
+    <div class={cn("flex flex-col gap-y-2.5", marginBottom)}>
+      <img
+        src={src}
+        class={cn(width, height, "object-cover")}
+        alt={alt ? alt : undefined}
+      />
+      {source ? (
+        <a class="ml-auto pr-10 text-sd-black dark:text-sd-brcyan underline" href={source} target="_blank" rel="noopener">
+          {alt}
+        </a>
+      ) : null}
+    </div>
+  );
 };
 
 export default Image;
