f30a6f89377c7983ae8bc7f7e1f7449eeba783f4
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-07-07 10:32:01 +0800

    feat: update banner

--- src/components/NewPostBanner/NewPostBanner.tsx
+++ src/components/NewPostBanner/NewPostBanner.tsx
@@ -1,28 +1,31 @@
 import { Component } from "solid-js";
 import { Nullable } from "../../types/general";
 import cn from "clsx";
+import { getFormattedTime } from "../../utils";
 
 export type NewPostBannerProps = {
   title: string;
   url: string;
   position: Nullable<string>;
+  updateDate: string;
 };
 
 const NewPostBanner: Component<NewPostBannerProps> = ({
   title,
   url,
   position,
+  updateDate,
 }) => {
   return (
-    <div class={cn("flex flex-row gap-x-5", position)}>
-      <div class="flex rounded-md bg-sd-yellow px-2.5 py-0.5">
+    <div class={cn("flex flex-row gap-x-10", position)}>
+      <div class="my-auto flex rounded-md bg-sd-yellow px-3 py-1">
         <p class="m-auto font-sans text-sm text-sd-brwhite">new</p>
       </div>
-      <a
-        href={url}
-        class="font-sans text-lg text-sd-brgreen underline dark:text-sd-brblue"
-      >
-        {title}
+      <a href={url} class="flex flex-col gap-y-1 font-sans">
+        <p class="text-lg text-sd-brgreen underline dark:text-sd-brblue">
+          {title}
+        </p>
+        <p class="text-sm text-sd-brblue">{getFormattedTime(updateDate)}</p>
       </a>
     </div>
   );
