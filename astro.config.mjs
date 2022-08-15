b3328752c56b6f82a04b72c191ade5ddf98103f5
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-08-15 11:21:57 +0800

    feat: Implement twitter and youtube embed remark plugin (#116)
    * refactor: cleanup code
    
    * feat: add deps
    
    * feat: add remark-twitter
    
    * feat: add remark-youtube
    
    * fix: fix unicode issue
    
    * feat: update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,17 +4,31 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 
 import prefetch from "@astrojs/prefetch";
+import remarkDirective from "remark-directive";
+import { remarkYoutube } from "./src/lib/markdown/remark-youtube.mjs";
+import { remarkTwitter } from "./src/lib/markdown/remark-twitter.mjs";
+import remarkRehype from "remark-rehype";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.summerbud.org",
-  integrations: [solid(), tailwind({
-    config: {
-      path: "./tailwind.config.cjs"
-    }
-  }), sitemap(), prefetch()],
+  integrations: [
+    solid(),
+    tailwind({
+      config: {
+        path: "./tailwind.config.cjs",
+      },
+    }),
+    sitemap(),
+    prefetch(),
+  ],
   markdown: {
-    // Can be 'shiki' (default), 'prism' or false to disable highlighting
-    syntaxHighlight: "prism"
-  }
+    //syntaxHighlight: "prism",
+    remarkPlugins: [
+      remarkDirective,
+      [remarkYoutube, { validateYoutubeLink: true }],
+      remarkTwitter,
+      remarkRehype,
+    ],
+  },
 });
