cca42fa3dc1f786c37420c7e96eaa41b55596bb2
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-08-26 16:01:44 +0800

    feat: add satro/image

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from "astro/config";
 import solid from "@astrojs/solid-js";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-
 import prefetch from "@astrojs/prefetch";
 import remarkDirective from "remark-directive";
 import { remarkYoutube } from "./src/lib/markdown/remark-youtube.mjs";
@@ -10,27 +9,20 @@ import { remarkTwitter } from "./src/lib/markdown/remark-twitter.mjs";
 import remarkRehype from "remark-rehype";
 import remarkGfm from "remark-gfm";
 
+import image from "@astrojs/image";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.summerbud.org",
-  integrations: [
-    solid(),
-    tailwind({
-      config: {
-        path: "./tailwind.config.cjs",
-      },
-    }),
-    sitemap(),
-    prefetch(),
-  ],
+  integrations: [solid(), tailwind({
+    config: {
+      path: "./tailwind.config.cjs"
+    }
+  }), sitemap(), prefetch(), image()],
   markdown: {
     //syntaxHighlight: "prism",
-    remarkPlugins: [
-      remarkGfm,
-      remarkDirective,
-      [remarkYoutube, { validateYoutubeLink: true }],
-      remarkTwitter,
-      remarkRehype,
-    ],
-  },
+    remarkPlugins: [remarkGfm, remarkDirective, [remarkYoutube, {
+      validateYoutubeLink: true
+    }], remarkTwitter, remarkRehype]
+  }
 });
