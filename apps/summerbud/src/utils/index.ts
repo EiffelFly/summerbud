dae95c07ff2363686d4cd2a5b1139e8eb09982f0
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2023-05-13 23:25:02 +0800

    feat: add getFormattedTime

--- apps/summerbud/src/utils/index.ts
+++ apps/summerbud/src/utils/index.ts
@@ -1,3 +1,10 @@
 export function getRandomInt(min: number, max: number) {
 	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+export function getFormattedTime(date: string) {
+	const dateObject = new Date(date);
+	const dateList = dateObject.toDateString().split(" ").slice(1);
+	const yearAndDate = [dateList[1], dateList[2]].join(", ");
+	return [dateList[0], yearAndDate].join(" ");
+}
