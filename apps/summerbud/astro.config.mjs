d48c028e3010d30e6893679178739eb33a257c74
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2023-05-13 23:47:46 +0800

    feat: add astro sitemap

--- apps/summerbud/astro.config.mjs
+++ apps/summerbud/astro.config.mjs
@@ -2,10 +2,12 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 import prefetch from "@astrojs/prefetch";
-
 import solidJs from "@astrojs/solid-js";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), tailwind(), prefetch(), solidJs()]
+	integrations: [mdx(), tailwind(), prefetch(), solidJs(), sitemap()],
+	site: "https://www.summerbud.org",
 });
