a2c3c90d5ce9fab9386c9f4eef274ab1e8c89749
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2023-05-13 23:51:02 +0800

    feat: add astro Image integration

--- apps/summerbud/astro.config.mjs
+++ apps/summerbud/astro.config.mjs
@@ -3,11 +3,12 @@ import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 import prefetch from "@astrojs/prefetch";
 import solidJs from "@astrojs/solid-js";
-
 import sitemap from "@astrojs/sitemap";
+
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [mdx(), tailwind(), prefetch(), solidJs(), sitemap()],
+	integrations: [mdx(), tailwind(), prefetch(), solidJs(), sitemap(), image()],
 	site: "https://www.summerbud.org",
 });
