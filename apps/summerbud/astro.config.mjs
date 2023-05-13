eea212ab6ecac0eb84c7306a0451fa382244b04e
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2023-05-13 18:04:05 +0800

    feat: add solidjs and clsx

--- apps/summerbud/astro.config.mjs
+++ apps/summerbud/astro.config.mjs
@@ -3,7 +3,9 @@ import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 import prefetch from "@astrojs/prefetch";
 
+import solidJs from "@astrojs/solid-js";
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [mdx(), tailwind(), prefetch()],
+  integrations: [mdx(), tailwind(), prefetch(), solidJs()]
 });
