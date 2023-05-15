92da95e16b1ef215b3f04a1e54a4a2678762b0c5
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2023-05-15 14:03:23 +0800

    feat: add min and max into AppleSlider

--- apps/summerbud/src/components/AppleSlider/AppleSlider.tsx
+++ apps/summerbud/src/components/AppleSlider/AppleSlider.tsx
@@ -2,10 +2,12 @@ import { createSignal } from "solid-js";
 import "./AppleSlider.css";
 
 export const AppleSlider = () => {
-	const [sliderValue, setSliderValue] = createSignal(0);
+	const [sliderValue, setSliderValue] = createSignal(50);
 	const [progressTrackColor, setProgressTrackColor] = createSignal("rgba(255, 255, 255, 0.8)");
 
 	const trackColor = "rgba(255, 255, 255, 0.5)";
+	const min = 10;
+	const max = 200;
 
 	return (
 		<div>
@@ -15,15 +17,17 @@ export const AppleSlider = () => {
 				onInput={(e) => {
 					setSliderValue(parseInt(e.target.value));
 				}}
-				onMouseEnter={() => setProgressTrackColor("rgba(255, 255, 255, 1)")}
-				onMouseLeave={() => setProgressTrackColor("rgba(255, 255, 255, 0.8)")}
+				onMouseDown={() => setProgressTrackColor("rgba(255, 255, 255, 1)")}
+				onMouseUp={() => setProgressTrackColor("rgba(255, 255, 255, 0.8)")}
 				value={sliderValue()}
 				style={{
 					"background-image": `linear-gradient(to right, 
             ${progressTrackColor()} 0%, ${progressTrackColor()} 
-            ${sliderValue()}%, ${trackColor}
-						${sliderValue()}%, ${trackColor} 100%)`,
+            ${((sliderValue() - min) / (max - min)) * 100}%, ${trackColor}
+						${((sliderValue() - min) / (max - min)) * 100}%, ${trackColor} 100%)`,
 				}}
+				min={min}
+				max={max}
 			/>
 		</div>
 	);
