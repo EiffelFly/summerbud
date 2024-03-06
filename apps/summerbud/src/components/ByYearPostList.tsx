b39b791b7e0a3017341cb19a3b47cdf730a5143d
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2024-03-06 16:40:13 +0800

    feat: add language header

--- apps/summerbud/src/components/ByYearPostList.tsx
+++ apps/summerbud/src/components/ByYearPostList.tsx
@@ -17,9 +17,18 @@ export const ByYearPostList = (props: ByYearPostListProps) => {
 							<a
 								href={`/${post.collection}/${post.slug}`}
 								rel="prefetch"
-								class="flex flex-col gap-y-1 font-sans text-base font-normal"
+								class="flex flex-col gap-y-2 font-sans text-base font-normal"
 							>
-								<span class="text-gray-500">{getFormattedTime(post.data.publishedAt)}</span>
+								<div class="flex flex-row gap-x-2">
+									<div class="flex px-4 grow-0 shrink-0 border border-gray-700">
+										<p class="font-sans text-gray-400 text-[12px] font-normal text-right mb-auto mr-auto">
+											{post.data.locale === "zh-TW" ? "ZH" : "EN"}
+										</p>
+									</div>
+									<span class="text-gray-500 mt-auto">
+										{getFormattedTime(post.data.publishedAt)}
+									</span>
+								</div>
 								<span class="hover:underline text-gray-300">{post.data.title}</span>
 							</a>
 						))}
